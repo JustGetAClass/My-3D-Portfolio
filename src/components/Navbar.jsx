12f92c13e317bfeed67ab65af6d9c22b262c9dc7
JustGetAClass <mohamedsalat62@gmail.com>
2023-05-04 13:59:21 +0300

    added my logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { styles } from "../style";
+import { navLinks } from "../constants";
+import { mylogo, menu, close } from "../assets";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav
+      className={`${styles.paddingX} fixed top-0 z-20 flex w-full items-center bg-primary py-5`}
+    >
+      <div className=" mx-auto flex w-full max-w-7xl items-center justify-between">
+        <Link
+          to="/"
+          className=" flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scroll(0, 0);
+          }}
+        >
+          <img src={mylogo} alt="logo" className=" h-9 w-9 object-contain" />
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
