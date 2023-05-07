f013c706fc4f59a3464e0b3c91c6e530aedfb959
JustGetAClass <mohamedsalat62@gmail.com>
2023-05-07 16:50:43 +0300

    updating experiences

--- src/constants/index.js
+++ src/constants/index.js
@@ -118,7 +118,7 @@ const experiences = [
     company_name: "Microsoft",
     icon: microsoft,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "January 2022 - June 2022",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
